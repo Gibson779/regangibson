d7e1419ecf3dbe23da9da503f10135381914e8c3
rgib <regangib@btinternet.com>
2025-11-01 17:44:08 +0000

    New Commit

---
+++ src/components/Me.jsx
@@ -0,0 +1,15 @@
+export default function Me(){
+    return(
+        <>
+        <div className="me-img">
+            <img className="regan" src="../images/ReganGibson.jpg" alt="Regan Gibson" />
+             <p1 className="Me">Hi I'm Regan a wedding photographer inspired by the worlds of art, fashion, and human connection. My work blends documentary observation with editorial styling, creating imagery that feels intentional, elegant, and deeply personal.
+                               Every wedding tells its story through design, emotion, and atmosphere. I look for the balance between those – the way light falls, fabric moves, and moments unfold – capturing it all with a fashion-inspired eye.
+                               I operate from my own creative space in the heart of Derby; Brickwork Studio, and also run Another Idea, a podcast and YouTube channel that celebrates and discusses the creative industry.
+        </p1>
+        </div>
+        </>
+    ) 
+}
+
+
