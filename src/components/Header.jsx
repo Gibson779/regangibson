992a00dfbebf92e7752f4a70289ba656de490b94
rgib <regangib@btinternet.com>
2025-11-01 21:20:52 +0000

    New commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,20 @@
 export default function Header(){
     return(
-      <header>
-        <h1>REGAN GIBSON</h1>
+      <>
+      <header className="header-entry">
+        <div className="main-header-container">
+        <h1 className="header-title">REGAN GIBSON</h1>
+        </div>
+          <div className="navbar">
+          <h1>Home</h1>
+          <h1>About</h1>
+          <h1>Contact</h1>
+          </div>
+        
         </header>
+        
+      </>
+      
     )
 }
 
