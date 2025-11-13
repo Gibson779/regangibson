5c8dc215f56e7c9870ae0564519c48abf74a1adb
rgib <regangib@btinternet.com>
2025-11-13 21:59:32 +0000

    NewComit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,24 @@
 export default function Footer() {
     return(
         <footer >
+            <>
+
+            <div className="links">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+            <div className="copyright">
+
             <p> © copyright 2025 Regan Gibson  All Rights Reserved. </p>
             
             <p>Developer Regan Gibosn</p>
 
+            </div>
+
+            </>
         </footer>
         
     )
